f58ad7da6201fdafc47985eb81ef8759fda80692
shannontorcato <shannon.torcato1234@gmail.com>
2022-04-21 20:47:34 -0400

    script to deploy on local network

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/iYmYpEJx-xO8qRAQY2zs02KH7t0hx1pu",
+      accounts: ["556dbd635c66c5ee8c9271abbc9277bdde0b901cdbd24de09658d2fd8a229862"]
+    },
+  },
 };
